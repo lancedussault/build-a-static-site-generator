1b27cb587943cc64305e155debd041a5bd8318b5
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-09-19 22:02:54 -0600

    created a LeafNode child class with tests

--- src/leafnode.py
+++
@@ -1,4 +0,0 @@
-from htmlnode import HTMLNode
-
-class LeafNode(HTMLNode):
-    def __init__
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,7 +17,22 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("div", None, None, {"data-info": 'He said, "Hello!"', "class": "example"})
         expected = ' data-info="He said, "Hello!"" class="example"'
         self.assertEqual(node.props_to_html(), expected)
-       
 
+    def test_value_only(self):
+        node = LeafNode(None, "I'm just a lil old value")
+        self.assertEqual(node.to_html(), "I'm just a lil old value")
+
+    def test_value_and_tag(self):
+        node = LeafNode("p", "I'm a paragraph")
+        self.assertEqual(node.to_html(), "<p>I'm a paragraph</p>")
+
+    def test_value_tag_prop(self):
+        node = LeafNode("a", "Click here", {"href": "https://www.fake-site.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.fake-site.com">Click here</a>')
+
+    def test_multiple_props(self):
+        node = LeafNode("a", "Open in new tab", {"href": "https://www.example.com", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<a href="https://www.example.com" target="_blank">Open in new tab</a>')
+       
 if __name__ == "__main__":
     unittest.main()
